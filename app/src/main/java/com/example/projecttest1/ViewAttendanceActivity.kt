1ea228e2c4fa473797731ad7acf3341f59220850
Admin <js915290@gmail.com>
2019-05-19 00:01:45 +0530

    student changes files
    Signed-off-by: Admin <js915290@gmail.com>

--- app/src/main/java/com/example/projecttest1/ViewAttendanceActivity.kt
+++ app/src/main/java/com/example/projecttest1/ViewAttendanceActivity.kt
@@ -1,12 +1,48 @@
 package com.example.projecttest1
 
+import android.app.DatePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_viewattendance.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ViewAttendanceActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_viewattendance)
+
+//calendar
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        From_date.setOnClickListener {
+
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+                // Display Selected date in textbox
+                From_date.setText("" + dayOfMonth + " / " + monthOfYear + " / " + year)
+            }, year, month, day)
+
+            dpd.show()
+        }
+
+        To_date.setOnClickListener {
+
+            val dpd1 = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+                // Display Selected date in textbox
+                To_date.setText("" + dayOfMonth + " / " + monthOfYear + " / " + year)
+            }, year, month, day)
+
+            dpd1.show()
+        }
     }
 }
